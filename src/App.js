be09b1e969371ce7bf7e8aed1516df5c3a38c0b1
Jamie <jamiejarrett2000@gmail.com>
2021-08-01 17:25:43 +0100

    Entire Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
 
 function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false);
+  const [model, setModel] = useState(null);
+  const [imageURL, setImageURL] = useState(null);
+  const [results, setResults] = useState([]);
+  const [history, setHistory] = useState([]);
+
+  const imageRef = useRef();
+  const textInputRef = useRef();
+  const fileInputRef = useRef();
+
+  const loadModel = async () => {
+    setIsModelLoading(true);
+    try {
+      const model = await mobilenet.load();
+      setModel(model);
+      setIsModelLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsModelLoading(false);
+    }
+  };
+
+  const uploadImage = (e) => {
+    const { files } = e.target;
+    if (files.length > 0) {
+      const url = URL.createObjectURL(files[0]);
+      setImageURL(url);
+    } else {
+      setImageURL(null);
+    }
+  };
+
+  const identify = async () => {
+    const results = await model.classify(imageRef.current);
+    setResults(results);
+  };
+
+  const triggerUpload = () => {
+    fileInputRef.current.click();
+  };
+
+  useEffect(() => {
+    loadModel();
+  }, []);
+
+  useEffect(() => {
+    if (imageURL) {
+      setHistory([imageURL, ...history]);
+    }
+  }, [imageURL]);
+
+  if (isModelLoading) {
+    return <h2>Model Loading...</h2>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <a className="navbar-brand" href="/pathfinding-visualizer">
+          <b>7 Days, 7 Projects - TensorFlow Image Identification (Day 7/7)</b>
         </a>
-      </header>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="https://jj1.dev/projects">
+                More Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <a
+        target="_blank"
+        href="https://github.com/JamieJarrettJJ/tensorflow-img-detect"
+        class="github-corner"
+        aria-label="View source on GitHub"
+      >
+        <svg
+          width="80"
+          height="80"
+          viewBox="0 0 250 250"
+          style={{
+            fill: "rgb(21 21 19 / 29%)",
+            color: "#fff",
+            position: "absolute",
+            top: "55",
+            border: "0",
+            left: "0",
+            transform: "scale(-1, 1)",
+          }}
+          aria-hidden="true"
+        >
+          <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+          <path
+            d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
+            fill="currentColor"
+            style={{ transformOrigin: "130px 106px" }}
+            class="octo-arm"
+          ></path>
+          <path
+            d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
+            fill="currentColor"
+            class="octo-body"
+          ></path>
+        </svg>
+      </a>
+      <div className="inputHolder">
+        <input
+          type="file"
+          accept="image/*"
+          capture="camera"
+          className="uploadInput"
+          onChange={uploadImage}
+          ref={fileInputRef}
+        />
+        <button className="uploadImage" onClick={triggerUpload}>
+          Upload Image
+        </button>
+      </div>
+      <div className="mainWrapper">
+        <div className="mainContent">
+          <div className="imageHolder">
+            {imageURL && (
+              <img
+                src={imageURL}
+                alt="Upload Preview"
+                crossOrigin="anonymous"
+                ref={imageRef}
+              />
+            )}
+          </div>
+          {results.length > 0 && (
+            <div className="resultsHolder">
+              {results.map((result, index) => {
+                return (
+                  <div className="result" key={result.className}>
+                    <span className="name">{result.className}</span>
+                    <span className="confidence">
+                      Confidence level: {(result.probability * 100).toFixed(2)}%{" "}
+                      {index === 0 && (
+                        <span className="bestGuess">Best Guess</span>
+                      )}
+                    </span>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        {imageURL && (
+          <button className="button" onClick={identify}>
+            Identify Image
+          </button>
+        )}
+      </div>
+      {history.length > 0 && (
+        <div className="recentPredictions">
+          <h2>Recent Images</h2>
+          <div className="recentImages">
+            {history.map((image, index) => {
+              return (
+                <div className="recentPrediction" key={`${image}${index}`}>
+                  <img
+                    src={image}
+                    alt="Recent Prediction"
+                    onClick={() => setImageURL(image)}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
